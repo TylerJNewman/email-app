ebecb9cd2d2db196114a72f66bc7a1bf23a1e43a
TylerJNewman <tylerjnewman18@gmail.com>
2018-11-28 15:42:06 -0800

    refactor to not save user if user exists in Mongo

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
+
+const User = mongoose.model("users");
 
 //console.developers.google.com
 passport.use(
@@ -11,9 +14,17 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("access token", accessToken);
-      console.log("refresh token", refreshToken);
-      console.log("profile", profile);
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          //we have record
+          done(null, existingUser);
+        } else {
+          //we don't have user record with this ID make new record
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
