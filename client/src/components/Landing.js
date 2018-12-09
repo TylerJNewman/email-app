ed970984897428df512740e57d2611a2f606be1e
TylerJNewman <tylerjnewman18@gmail.com>
2018-12-09 14:23:25 -0800

    happy with landing

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,11 +1,38 @@
 import React from "react";
+import "./Landing.css";
 
 const Landing = () => {
   return (
-    <div style={{ textAlign: "center" }}>
-      <h1>React-Email</h1>
-      Collect feedback from users
-    </div>
+    <section id="hero">
+      <div className="row">
+        <div className="col l6">
+          <div className="copy">
+            <h1>
+              <div className="step-trigger step-1 investing">Investing.</div>
+              <div className="step-2">Without the risk.</div>
+            </h1>
+            <h4 className="summary step-3">
+              S-Play lets you invest in the stock market with play money.
+            </h4>
+            <a
+              className="waves-effect waves-light btn blue darken-2"
+              href="/auth/google"
+            >
+              Login With Google
+            </a>
+          </div>
+        </div>
+        <div className="col l6">
+          <div className="copy">
+            <div className="card">
+              <div className="card-image">
+                <img src="img/ocean.jpg" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
